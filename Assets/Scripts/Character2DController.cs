dd761ed27a7266ce80fcef19790e6012596d046d
iceteaferahligi <oztas.serife@hotmail.com>
2021-10-07 15:10:28 +0300

    Character update
    - The player character and its asset changed. The player's skills and animations updated. (Walk- Jump etc.)
    - Water animation added.

--- Assets/Scripts/Character2DController.cs
+++ Assets/Scripts/Character2DController.cs
@@ -7,6 +7,7 @@ public class Character2DController : MonoBehaviour
     public float JumpForce = 10;
 
     private Rigidbody2D _rigidbody;
+    public Animator animator;
 
     private void Start()
     {
@@ -19,22 +20,30 @@ public class Character2DController : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
 
-        if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) 
+        animator.SetBool("Moving", false);
+        animator.SetBool("Jumping", false);
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) 
         {
             _rigidbody.AddForce(new Vector2(0,JumpForce),ForceMode2D.Impulse);
+            animator.SetBool("Jumping", true);
+            animator.SetBool("Moving", false);
+
         }
 
         //Karakterin yürürken saða sola dönmesini saðlar
-        if(!Mathf.Approximately(0,movement))
+        if (!Mathf.Approximately(0, movement))
         {
 
-            if(movement > 0)
+            if (movement < 0)
             {
                 transform.rotation = Quaternion.Euler(0, 180, 0);
+                animator.SetBool("Moving", true);
             }
             else
             {
                 transform.rotation = Quaternion.identity;
+                animator.SetBool("Moving", true);
             }
             //transform.rotation  = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
         }
--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -8,12 +8,15 @@ public class ChestScript : MonoBehaviour
     public static bool anim_played = false;
     public GameObject panel;
 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             myAnimationController.SetBool("playAnimation", true);
             panel.SetActive(true);
+            //Debug.Log(collision.gameObject.GetComponent<Rigidbody2D>().velocity.ToString());       
+    
         }
 
     }
